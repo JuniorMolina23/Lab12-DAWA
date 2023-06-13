fa3b1ce50bd88b98303480c99c96e3121b2bf108
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2023-06-12 21:46:13 -0500

    Laboratorio12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PostsModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
